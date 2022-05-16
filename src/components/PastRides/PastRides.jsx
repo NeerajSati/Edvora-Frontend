6b4307acb619f1ec38383367e4c94d7c8b4947eb
Neeraj Sati <neeraj.sati123@gmail.com>
2022-05-16 14:00:49 +0530

    Added past ride and upcoming rides

--- src/components/PastRides/PastRides.jsx
+++ src/components/PastRides/PastRides.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect}  from 'react'
 import Details from '../details/Details'
 import './pastrides.css'
 
 function PastRides({rides,user}) {
+  const [pastRides, setPastRides] = useState([]);
+  useEffect(() => {
+    const today = new Date();
+    const filtered = rides.filter((ride) => {
+      const date = new Date(ride.date);
+      return today > date;
+    });
+    setPastRides(filtered);
+  }, []);
   return (
     <div className="pastRides">
-    {rides.map((e) => {
+    {pastRides.length === 0 ? <p className='noContent'>No rides Here!</p> : (pastRides.map((e) => {
       return <Details ride={e} user={user}/>
-    })}
+    }))}
     </div>
   )
 }
--- src/components/UpcomingRides/UpcomingRides.jsx
+++ src/components/UpcomingRides/UpcomingRides.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Details from '../details/Details'
 import './upcomingrides.css'
 
 function UpcomingRides({rides,user}) {
+  const [upcomingRides, setUpcomingRides] = useState([]);
+  useEffect(() => {
+    const today = new Date();
+    const filtered = rides.filter((ride) => {
+      const date = new Date(ride.date);
+      return today < date;
+    });
+    setUpcomingRides(filtered);
+  }, []);
   return (
     <div className="upcomingRides">
-    {rides.map((e) => {
+    {upcomingRides.length === 0 ? <p className='noContent'>No rides Here!</p> : (upcomingRides.map((e) => {
       return <Details ride={e} user={user}/>
-    })}
+    }))}
     </div>
   )
 }
